4bca1dcf117c10576b4af9eabb2889d1e0b3c22e
Константин <kk0sta@MacBook-Pro-Konstantin-2.local>
2026-07-12 13:16:43 +0300

    chore: checkpoint before realtime integrity fixes

--- apps/frontend/dev-dist/sw.js
+++ apps/frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.16q8q1jaf6s"
+    "revision": "0.jan365dpids"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
